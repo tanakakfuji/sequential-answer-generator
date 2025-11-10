78271510f80b3a4ef4177b7c7663fc391668f944
Kiyoto Tanaka <tanaka.kiyoto56@gmail.com>
2025-11-10 10:23:33 +0900

    メッセージの組み立てと複数リクエストを実行する処理を追加

---
+++ src/generator.py
@@ -0,0 +1,53 @@
+from src.openai import send_messages as request_openai
+from src.ollama import send_messages as request_ollama
+import os
+from dotenv import load_dotenv
+import time
+
+load_dotenv(override=True)
+
+def generate(client, system_prompt, queries_list):
+  outputs = []
+  total_error = 0
+  continuous_error = 0
+  for queries in queries_list:
+    results = _execute_requests(client, system_prompt, queries)
+    # 連続したAPIエラーへの対処
+    if results[-1]['status'] == 'success':
+      continuous_error = 0
+    else:
+      total_error += 1
+      continuous_error += 1
+    if continuous_error == 5:
+      raise RuntimeError('API呼び出しが5回連続して失敗したため、処理を停止します')
+    for i in range(len(queries)):
+      outputs.append(queries[i]|results[i])
+  return outputs, total_error
+    
+def _execute_requests(client, system_prompt, queries):
+  results = []
+  messages = [{'role': 'system','content': system_prompt}]
+  for i, q in enumerate(queries):
+    messages.append({'role': 'user','content': q['query']})
+    if client == 'openai':
+      result = request_openai(messages)
+    elif client == 'ollama':
+      result = request_ollama(messages)
+    else:
+      raise ValueError('clientオプションの値が不正です')
+    time.sleep(float(os.getenv('REQUEST_INTERVAL_TIME')))
+    
+    if result['status'] == 'success':
+      results.append(result)
+      messages.append({'role': 'assistant','content': result['text']})
+    else:
+      # エラーの場合、以降の回答は空にする
+      for _ in range(len(queries) - i):
+        results.append(result)
+      break
+    # 古い対話履歴はメッセージから削除する
+    if i >= int(os.getenv('CONVERSATION_BUFFER_TURNS')) - 1:
+      messages.pop(2)
+      messages.pop(1)
+
+  return results
---
+++ tests/test_generator.py
@@ -0,0 +1,246 @@
+from src.generator import generate, _execute_requests
+import pytest
+from unittest.mock import Mock, patch, call, DEFAULT
+from copy import deepcopy
+
+SYSTEM_PROMPT = 'テストシステムプロンプト'
+QUERIES = [{'conversation_id': '1', 'query': '質問1'},{'conversation_id': '1', 'query': '質問2'},{'conversation_id': '1', 'query': '質問3'},{'conversation_id': '1', 'query': '質問4'}]
+QUERIES_LIST = [
+  [{'conversation_id': '1', 'query': 'テスト質問'}, {'conversation_id': '1', 'query': 'テスト質問'}],
+  [{'conversation_id': '2', 'query': 'テスト質問'}, {'conversation_id': '2', 'query': 'テスト質問'}],
+  [{'conversation_id': '3', 'query': 'テスト質問'}, {'conversation_id': '3', 'query': 'テスト質問'}],
+  [{'conversation_id': '4', 'query': 'テスト質問'}, {'conversation_id': '4', 'query': 'テスト質問'}],
+  [{'conversation_id': '5', 'query': 'テスト質問'}, {'conversation_id': '5', 'query': 'テスト質問'}],
+  [{'conversation_id': '6', 'query': 'テスト質問'}, {'conversation_id': '6', 'query': 'テスト質問'}],
+]
+
+def test_generate_without_errors():
+  with(patch('src.generator._execute_requests', return_value=[{'text': 'テスト回答', 'status': 'success'} for _ in range(len(QUERIES_LIST[0]))]) as mock_execute):
+    client = 'openai'
+    assert generate(client, SYSTEM_PROMPT, QUERIES_LIST) == (
+      [
+        {'conversation_id': '1', 'query': 'テスト質問', 'text': 'テスト回答', 'status': 'success'}, {'conversation_id': '1', 'query': 'テスト質問', 'text': 'テスト回答', 'status': 'success'},
+        {'conversation_id': '2', 'query': 'テスト質問', 'text': 'テスト回答', 'status': 'success'}, {'conversation_id': '2', 'query': 'テスト質問', 'text': 'テスト回答', 'status': 'success'},
+        {'conversation_id': '3', 'query': 'テスト質問', 'text': 'テスト回答', 'status': 'success'}, {'conversation_id': '3', 'query': 'テスト質問', 'text': 'テスト回答', 'status': 'success'},
+        {'conversation_id': '4', 'query': 'テスト質問', 'text': 'テスト回答', 'status': 'success'}, {'conversation_id': '4', 'query': 'テスト質問', 'text': 'テスト回答', 'status': 'success'},
+        {'conversation_id': '5', 'query': 'テスト質問', 'text': 'テスト回答', 'status': 'success'}, {'conversation_id': '5', 'query': 'テスト質問', 'text': 'テスト回答', 'status': 'success'},
+        {'conversation_id': '6', 'query': 'テスト質問', 'text': 'テスト回答', 'status': 'success'}, {'conversation_id': '6', 'query': 'テスト質問', 'text': 'テスト回答', 'status': 'success'},
+      ],
+      0
+    )
+    mock_execute.assert_has_calls(calls=[
+      call(client, SYSTEM_PROMPT, [{'conversation_id': '1', 'query': 'テスト質問'}, {'conversation_id': '1', 'query': 'テスト質問'}]),
+      call(client, SYSTEM_PROMPT, [{'conversation_id': '2', 'query': 'テスト質問'}, {'conversation_id': '2', 'query': 'テスト質問'}]),
+      call(client, SYSTEM_PROMPT, [{'conversation_id': '3', 'query': 'テスト質問'}, {'conversation_id': '3', 'query': 'テスト質問'}]),
+      call(client, SYSTEM_PROMPT, [{'conversation_id': '4', 'query': 'テスト質問'}, {'conversation_id': '4', 'query': 'テスト質問'}]),
+      call(client, SYSTEM_PROMPT, [{'conversation_id': '5', 'query': 'テスト質問'}, {'conversation_id': '5', 'query': 'テスト質問'}]),
+      call(client, SYSTEM_PROMPT, [{'conversation_id': '6', 'query': 'テスト質問'}, {'conversation_id': '6', 'query': 'テスト質問'}]),
+    ])
+
+def test_generate_with_api_error():
+  with(patch('src.generator._execute_requests') as mock_execute):
+    mock_execute.side_effect = [
+      [{'text': 'テスト回答', 'status': 'success'} for _ in range(len(QUERIES_LIST[0]))],
+      [{'text': '', 'status': 'API_ERROR'} for _ in range(len(QUERIES_LIST[0]))],
+      [{'text': 'テスト回答', 'status': 'success'} for _ in range(len(QUERIES_LIST[0]))],
+      [{'text': '', 'status': 'API_ERROR'} for _ in range(len(QUERIES_LIST[0]))],
+      [{'text': 'テスト回答', 'status': 'success'} for _ in range(len(QUERIES_LIST[0]))],
+      [{'text': '', 'status': 'API_ERROR'} for _ in range(len(QUERIES_LIST[0]))]
+    ]
+    client = 'openai'
+    assert generate(client, SYSTEM_PROMPT, QUERIES_LIST) == (
+      [
+        {'conversation_id': '1', 'query': 'テスト質問', 'text': 'テスト回答', 'status': 'success'}, {'conversation_id': '1', 'query': 'テスト質問', 'text': 'テスト回答', 'status': 'success'},
+        {'conversation_id': '2', 'query': 'テスト質問', 'text': '', 'status': 'API_ERROR'}, {'conversation_id': '2', 'query': 'テスト質問', 'text': '', 'status': 'API_ERROR'},
+        {'conversation_id': '3', 'query': 'テスト質問', 'text': 'テスト回答', 'status': 'success'}, {'conversation_id': '3', 'query': 'テスト質問', 'text': 'テスト回答', 'status': 'success'},
+        {'conversation_id': '4', 'query': 'テスト質問', 'text': '', 'status': 'API_ERROR'}, {'conversation_id': '4', 'query': 'テスト質問', 'text': '', 'status': 'API_ERROR'},
+        {'conversation_id': '5', 'query': 'テスト質問', 'text': 'テスト回答', 'status': 'success'}, {'conversation_id': '5', 'query': 'テスト質問', 'text': 'テスト回答', 'status': 'success'},
+        {'conversation_id': '6', 'query': 'テスト質問', 'text': '', 'status': 'API_ERROR'}, {'conversation_id': '6', 'query': 'テスト質問', 'text': '', 'status': 'API_ERROR'},
+      ],
+      3
+    )
+    mock_execute.assert_has_calls(calls=[
+      call(client, SYSTEM_PROMPT, [{'conversation_id': '1', 'query': 'テスト質問'}, {'conversation_id': '1', 'query': 'テスト質問'}]),
+      call(client, SYSTEM_PROMPT, [{'conversation_id': '2', 'query': 'テスト質問'}, {'conversation_id': '2', 'query': 'テスト質問'}]),
+      call(client, SYSTEM_PROMPT, [{'conversation_id': '3', 'query': 'テスト質問'}, {'conversation_id': '3', 'query': 'テスト質問'}]),
+      call(client, SYSTEM_PROMPT, [{'conversation_id': '4', 'query': 'テスト質問'}, {'conversation_id': '4', 'query': 'テスト質問'}]),
+      call(client, SYSTEM_PROMPT, [{'conversation_id': '5', 'query': 'テスト質問'}, {'conversation_id': '5', 'query': 'テスト質問'}]),
+      call(client, SYSTEM_PROMPT, [{'conversation_id': '6', 'query': 'テスト質問'}, {'conversation_id': '6', 'query': 'テスト質問'}]),
+    ])
+
+def test_generate_continuous_api_errors():
+  client = 'openai'
+  with (
+    patch('src.generator._execute_requests', return_value=[{'text': '', 'status': 'API_ERROR'} for _ in range(len(QUERIES_LIST[0]))]) as mock_execute,
+    pytest.raises(RuntimeError, match='API呼び出しが5回連続して失敗したため、処理を停止します')
+  ):
+    generate(client, SYSTEM_PROMPT, QUERIES_LIST)
+    mock_execute.assert_has_calls(calls=[
+      call(client, SYSTEM_PROMPT, [{'conversation_id': '1', 'query': 'テスト質問'}, {'conversation_id': '1', 'query': 'テスト質問'}]),
+      call(client, SYSTEM_PROMPT, [{'conversation_id': '2', 'query': 'テスト質問'}, {'conversation_id': '2', 'query': 'テスト質問'}]),
+      call(client, SYSTEM_PROMPT, [{'conversation_id': '3', 'query': 'テスト質問'}, {'conversation_id': '3', 'query': 'テスト質問'}]),
+      call(client, SYSTEM_PROMPT, [{'conversation_id': '4', 'query': 'テスト質問'}, {'conversation_id': '4', 'query': 'テスト質問'}]),
+      call(client, SYSTEM_PROMPT, [{'conversation_id': '5', 'query': 'テスト質問'}, {'conversation_id': '5', 'query': 'テスト質問'}]),
+      call(client, SYSTEM_PROMPT, [{'conversation_id': '6', 'query': 'テスト質問'}, {'conversation_id': '6', 'query': 'テスト質問'}]),
+    ])
+
+
+def test_execute_requests_openai_success(monkeypatch):
+  monkeypatch.setenv('CONVERSATION_BUFFER_TURNS', '2')
+  monkeypatch.setenv('REQUEST_INTERVAL_TIME', '5.0')
+  with (
+    patch('src.generator.request_openai', return_value={'text': 'テスト回答', 'status': 'success'}) as mock_request,
+    patch('src.generator.time.sleep') as mock_sleep
+  ):
+    mock_request = copy_call_args(mock_request)
+    client = 'openai'
+    assert _execute_requests(client, SYSTEM_PROMPT, QUERIES) == [
+      {'text': 'テスト回答', 'status': 'success'},
+      {'text': 'テスト回答', 'status': 'success'},
+      {'text': 'テスト回答', 'status': 'success'},
+      {'text': 'テスト回答', 'status': 'success'}
+    ]
+    mock_request.assert_has_calls(calls=[
+      call([
+        {'role': 'system', 'content': SYSTEM_PROMPT},
+        {'role': 'user', 'content': '質問1'},
+      ]),
+      call([
+        {'role': 'system', 'content': SYSTEM_PROMPT},
+        {'role': 'user', 'content': '質問1'},
+        {'role': 'assistant', 'content': 'テスト回答'},
+        {'role': 'user', 'content': '質問2'},
+      ]),
+      call([
+        {'role': 'system', 'content': SYSTEM_PROMPT},
+        {'role': 'user', 'content': '質問2'},
+        {'role': 'assistant', 'content': 'テスト回答'},
+        {'role': 'user', 'content': '質問3'},
+      ]),
+      call([
+        {'role': 'system', 'content': SYSTEM_PROMPT},
+        {'role': 'user', 'content': '質問3'},
+        {'role': 'assistant', 'content': 'テスト回答'},
+        {'role': 'user', 'content': '質問4'},
+      ])
+    ])
+    mock_sleep.assert_has_calls(calls=[
+      call(5.0),
+      call(5.0),
+      call(5.0),
+      call(5.0)
+    ])
+
+def test_execute_requests_ollama_success(monkeypatch):
+  monkeypatch.setenv('CONVERSATION_BUFFER_TURNS', '2')
+  monkeypatch.setenv('REQUEST_INTERVAL_TIME', '5.0')
+  with (
+    patch('src.generator.request_ollama', return_value={'text': 'テスト回答', 'status': 'success'}) as mock_request,
+    patch('src.generator.time.sleep') as mock_sleep
+  ):
+    mock_request = copy_call_args(mock_request)
+    client = 'ollama'
+    assert _execute_requests(client, SYSTEM_PROMPT, QUERIES) == [
+      {'text': 'テスト回答', 'status': 'success'},
+      {'text': 'テスト回答', 'status': 'success'},
+      {'text': 'テスト回答', 'status': 'success'},
+      {'text': 'テスト回答', 'status': 'success'}
+    ]
+    mock_request.assert_has_calls(calls=[
+      call([
+        {'role': 'system', 'content': SYSTEM_PROMPT},
+        {'role': 'user', 'content': '質問1'},
+      ]),
+      call([
+        {'role': 'system', 'content': SYSTEM_PROMPT},
+        {'role': 'user', 'content': '質問1'},
+        {'role': 'assistant', 'content': 'テスト回答'},
+        {'role': 'user', 'content': '質問2'},
+      ]),
+      call([
+        {'role': 'system', 'content': SYSTEM_PROMPT},
+        {'role': 'user', 'content': '質問2'},
+        {'role': 'assistant', 'content': 'テスト回答'},
+        {'role': 'user', 'content': '質問3'},
+      ]),
+      call([
+        {'role': 'system', 'content': SYSTEM_PROMPT},
+        {'role': 'user', 'content': '質問3'},
+        {'role': 'assistant', 'content': 'テスト回答'},
+        {'role': 'user', 'content': '質問4'},
+      ])
+    ])
+    mock_sleep.assert_has_calls(calls=[
+      call(5.0),
+      call(5.0),
+      call(5.0),
+      call(5.0)
+    ])
+
+def test_execute_requests_openai_api_error(monkeypatch):
+  monkeypatch.setenv('CONVERSATION_BUFFER_TURNS', '2')
+  monkeypatch.setenv('REQUEST_INTERVAL_TIME', '5.0')
+  with (
+    patch('src.generator.request_openai') as mock_request,
+    patch('src.generator.time.sleep') as mock_sleep
+  ):
+    mock_request.side_effect = [
+      {'text': 'テスト回答', 'status': 'success'},
+      {'text': 'テスト回答', 'status': 'success'},
+      {'text': 'テスト回答', 'status': 'success'},
+      {'text': '', 'status': 'API_ERROR'}
+    ]
+    client = 'openai'
+    assert _execute_requests(client, SYSTEM_PROMPT, QUERIES) == [
+      {'text': 'テスト回答', 'status': 'success'},
+      {'text': 'テスト回答', 'status': 'success'},
+      {'text': 'テスト回答', 'status': 'success'},
+      {'text': '', 'status': 'API_ERROR'}
+    ]
+    assert mock_request.call_count == 4
+    mock_sleep.assert_has_calls(calls=[
+      call(5.0),
+      call(5.0),
+      call(5.0),
+      call(5.0),
+    ])
+
+def test_execute_requests_ollama_api_error(monkeypatch):
+  monkeypatch.setenv('CONVERSATION_BUFFER_TURNS', '2')
+  monkeypatch.setenv('REQUEST_INTERVAL_TIME', '5.0')
+  with (
+    patch('src.generator.request_ollama') as mock_request,
+    patch('src.generator.time.sleep') as mock_sleep
+  ):
+    mock_request.side_effect = [
+      {'text': 'テスト回答', 'status': 'success'},
+      {'text': '', 'status': 'API_ERROR'},
+    ]
+    client = 'ollama'
+    assert _execute_requests(client, SYSTEM_PROMPT, QUERIES) == [
+      {'text': 'テスト回答', 'status': 'success'},
+      {'text': '', 'status': 'API_ERROR'},
+      {'text': '', 'status': 'API_ERROR'},
+      {'text': '', 'status': 'API_ERROR'}
+    ]
+    assert mock_request.call_count == 2
+    mock_sleep.assert_has_calls(calls=[
+      call(5.0),
+      call(5.0)
+    ])
+
+def test_execute_requests_client_invalid_value():
+  client = 'sample'
+  with pytest.raises(ValueError, match='clientオプションの値が不正です'):
+    _execute_requests(client, SYSTEM_PROMPT, QUERIES)
+
+# ミュータブルな引数をモックで適切に検証するために必要な関数（https://docs.python.org/ja/3/library/unittest.mock-examples.html#coping-with-mutable-arguments）
+def copy_call_args(mock):
+  new_mock = Mock()
+  def side_effect(*args, **kwargs):
+      args = deepcopy(args)
+      kwargs = deepcopy(kwargs)
+      new_mock(*args, **kwargs)
+      return DEFAULT
+  mock.side_effect = side_effect
+  return new_mock
