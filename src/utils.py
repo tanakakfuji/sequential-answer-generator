c258d9446c8dd80a3b370d8a9668a7dd547fe4fd
Kiyoto Tanaka <tanaka.kiyoto56@gmail.com>
2025-11-10 11:47:41 +0900

    ファイル読み取り・書き込み処理を追加

---
+++ src/utils.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+import csv
+import os
+
+def load_text(path):
+  file_path = Path(__file__).parent.parent / path
+  with open(file_path, encoding="utf-8") as f:
+    return f.read()
+
+def load_csv(path):
+  file_path = Path(__file__).parent.parent / path
+  with open(file_path, encoding="utf-8") as f:
+    reader = csv.reader(f, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
+    return list(reader)
+  
+def save_csv(data, dir_path, filename):
+  os.makedirs(dir_path, exist_ok=True)
+  file_path = Path(__file__).parent.parent / dir_path / filename
+  with open(file_path, mode='w', encoding='utf-8') as f:
+    writer = csv.DictWriter(f, fieldnames=data[0].keys(), delimiter=',', doublequote=True, lineterminator="\r\n", quotechar='"')
+    writer.writeheader()
+    writer.writerows(data)
