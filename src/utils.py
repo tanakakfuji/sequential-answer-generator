ff19ecd82dc1a3ff22d0f56a1035c1344ce43402
Kiyoto Tanaka <tanaka.kiyoto56@gmail.com>
2025-11-23 12:16:25 +0900

    エクセルに対応するためBOMを付加

--- src/utils.py
+++ src/utils.py
@@ -4,19 +4,19 @@ import os
 
 def load_text(path):
   file_path = Path(__file__).parent.parent / path
-  with open(file_path, encoding="utf-8") as f:
+  with open(file_path, encoding="utf-8-sig") as f:
     return f.read()
 
 def load_csv(path):
   file_path = Path(__file__).parent.parent / path
-  with open(file_path, encoding="utf-8") as f:
+  with open(file_path, encoding="utf-8-sig") as f:
     reader = csv.reader(f, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
     return list(reader)
   
 def save_csv(data, dir_path, filename):
   os.makedirs(dir_path, exist_ok=True)
   file_path = Path(__file__).parent.parent / dir_path / filename
-  with open(file_path, mode='w', encoding='utf-8') as f:
+  with open(file_path, mode='w', encoding='utf-8-sig') as f:
     writer = csv.DictWriter(f, fieldnames=data[0].keys(), delimiter=',', doublequote=True, lineterminator="\r\n", quotechar='"')
     writer.writeheader()
     writer.writerows(data)
